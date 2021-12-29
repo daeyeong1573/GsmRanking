1ff267ac36b937cd70a1b4d880c0813374cf4cfd
KDY <rlaeodud9983@naver.com>
2021-12-29 20:40:47 +0900

    [CREATE] 로그인 화면 생성

---
+++ app/src/main/java/org/gsm/software/gsmranking/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package org.gsm.software.gsmranking.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.gsm.software.gsmranking.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
