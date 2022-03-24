56e7f7363d0436ca6c7b521e09d47f641a7aa621
KDY <rlaeodud9983@naver.com>
2022-03-24 21:07:50 +0900

    [UPDATE] 정렬순서 기존 : contributions 하나에서 star와 fork를 추가하기 위해 String형식의 criteria 생성

--- app/src/main/java/org/gsm/software/gsmranking/model/repository/RankingRepository.kt
+++ app/src/main/java/org/gsm/software/gsmranking/model/repository/RankingRepository.kt
@@ -11,7 +11,7 @@ import javax.inject.Inject
 @Module
 @InstallIn(SingletonComponent::class)
 class RankingRepository @Inject constructor (val api : ApolloClient) {
-    fun getRanking(generation : Int) : ApolloCall<RankingResultQuery.Data>{
-        return api.query(RankingResultQuery(criteria = "contributions", 0, page = 1, generation))
+    fun getRanking(generation : Int, criteria : String) : ApolloCall<RankingResultQuery.Data>{
+        return api.query(RankingResultQuery(criteria = criteria , 0, page = 1, generation))
     }
 }
